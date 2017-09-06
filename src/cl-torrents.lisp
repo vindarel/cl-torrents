f236aaebffbd71733bea71100e59f4f392b592f4
vindarel <ehvince@mailz.org>
2017-09-06 16:47:45 +0200

    glue functions to get the magnet link of a search result.

--- src/cl-torrents.lisp
+++ src/cl-torrents.lisp
@@ -1,7 +1,8 @@
 (in-package :cl-user)
 (defpackage cl-torrents
   (:use :cl)
-  (:export :torrents))
+  (:export :torrents
+           :magnet))
 ;; to do: shadow-import to use search as a funnction name.
 (in-package :cl-torrents)
 
@@ -11,6 +12,8 @@
 
 (defparameter *selectors* ".Title")
 
+(defvar *last-search* nil "Remembering the last search (should be an hash-map).")
+
 (defun torrents (words)
   "Search torrents."
   (let* ((terms (str:words words))
@@ -18,8 +21,10 @@
          (*search-url* (str:replace-all "{KEYWORDS}" query *search-url*))
          (req (dex:get *search-url*))
          (html (plump:parse req))
-         (res (lquery:$ html *selectors*)))
-    (coerce res 'list)))
+         (res (lquery:$ html *selectors*))
+         (res-list (coerce res 'list)))
+    (setf *last-search* res-list)
+    res-list))
 
 (defun detail-page-url (node)
   "Extract the link of the details page. `node': plump node, containing the url."
@@ -30,12 +35,23 @@
 ;; test:
 ;; (mapcar #'detail-page-url (torrents "matrix"))
 
-(defun magnet-link (node)
+(defun find-magnet-link (parsed)
   "Extract the magnet link."
   (let* ((hrefs (mapcar (lambda (it)
                           (lquery-funcs:attr it "href"))
-                        (coerce (lquery:$ node "a") 'list)))
+                        (coerce (lquery:$ parsed "a") 'list)))
          (magnet (remove-if-not (lambda (it)
                                   (str:starts-with? "magnet" it))
                                 hrefs)))
     (first magnet)))
+
+(defun magnet-link-from (node)
+  "Extract the magnet link from a `torrent' result."
+  (let* ((url (detail-page-url node))
+         (html (dex:get url))
+         (parsed (plump:parse html)))
+    (find-magnet-link parsed)))
+
+(defun magnet (index)
+  "Search the magnet from last search's `index''s result."
+  (magnet-link-from (elt *last-search* index)))
